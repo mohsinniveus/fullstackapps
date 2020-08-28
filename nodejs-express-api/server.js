2c95fbb29bf89bd7b4ee614b183ef1fe73accb97
atmohsin <atmohsin@penguin>
2020-08-28 15:18:53 +0530

    log4j added

--- nodejs-express-api/server.js
+++ nodejs-express-api/server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose")
 const userRouter = require("./routes/user");
 const bodyParser = require('body-parser');
+var log4js = require("log4js");
+
+log4js.configure({
+  appenders: { nodeapi: { type: "file", filename: "output.log" } },
+  categories: { default: { appenders: ["nodeapi"], level: "info" } }
+});
 
 
 //Starting point of server
 function main() {
+  const logger = log4js.getLogger("nodeapi");
   let app = express();
   const port = process.env.PORT || 5000;
   app.use(bodyParser.urlencoded({ // Middleware
@@ -16,7 +23,8 @@ function main() {
   mongoose
   .connect("mongodb://localhost:27017/test", { useNewUrlParser: true,useUnifiedTopology: true })
   .then(() => {
-     console.log("Mongo database is connected...."); 
+     console.log("Mongo database is connected....");
+     logger.info("Mongo database is started....."); 
   });
 
   // Routes & Handlers
